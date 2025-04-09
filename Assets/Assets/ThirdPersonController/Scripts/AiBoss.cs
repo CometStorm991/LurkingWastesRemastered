c6d2888d817f671f8af64edd96dc1104c60fa132
CometStorm991 <cometstorm991@gmail.com>
2025-04-08 21:18:54 -0400

    Added basic AI

---
+++ Assets/Assets/ThirdPersonController/Scripts/AiBoss.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+
+public class AiBoss : MonoBehaviour
+{
+    public Rigidbody rigidbody;
+
+    public GameObject player;
+
+    public float walkingSpeed;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float x = transform.position.x;
+        float z = transform.position.z;
+        float playerX = player.transform.position.x;
+        float playerZ = player.transform.position.z;
+
+        Vector3 walkingForce = Vector3.zero;
+        walkingForce += (new Vector3(walkingSpeed, 0.0f, 0.0f)) * (x > playerX ? -1.0f : 1.0f);
+        walkingForce += (new Vector3(0.0f, 0.0f, walkingSpeed)) * (z > playerZ ? -1.0f : 1.0f);
+
+        rigidbody.AddForce(walkingForce);
+    }
+}
